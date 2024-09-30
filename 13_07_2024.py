b23f7183dee291341b2f2128f649cefaf98ee16b
stasjem <stasjem@gmail.com>
2024-09-30 19:55:24 +0400

    Create 13_07_2024.py

---
+++ 13_07_2024.py
@@ -0,0 +1,6 @@
+
+from figures import circle_area, triangle_perimeter, square_area
+
+print(circle_area(10))  # Площадь окружности с радиусом 10
+print(triangle_perimeter())  # Периметр треугольника со сторонами по умолчанию
+print(square_area(5))  # Площадь квадрата со стороной 5
