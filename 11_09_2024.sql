775d2b3e755c5a0a9307bebbe2c0484155893688
stasjem <stasjem@gmail.com>
2024-09-30 21:05:03 +0400

    Create 11_09_2024.sql

---
+++ 11_09_2024.sql
@@ -0,0 +1,16 @@
+--11.09
+-- 1. Выбрать все данные из таблицы customers
+-- 2. Выбрать все записи из таблицы customers, но только колонки "имя контакта" и "город"
+-- 3. Выбрать все записи из таблицы orders, но взять две колонки: идентификатор заказа и колонку, значение в которой мы рассчитываем как разницу между датой отгрузки и датой формирования заказа.
+-- 4. Выбрать все уникальные города в которых "зарегестрированы" заказчики
+-- 5. Выбрать все уникальные сочетания городов и стран в которых "зарегестрированы" заказчики
+-- 6. Посчитать кол-во заказчиков
+-- 7. Посчитать кол-во уникальных стран в которых "зарегестрированы" заказчики
+
+--1 SELECT * FROM customers;
+--2 SELECT contact_name, city FROM customers;
+--3 SELECT order_id, DATEDIFF(ship_date, order_date) AS days_between FROM orders;\
+--4 SELECT DISTINCT city FROM customers;
+--5 SELECT DISTINCT city, country FROM customers;
+--6 SELECT COUNT(*) AS total_customers FROM customers;
+--7 SELECT COUNT(DISTINCT country) AS unique_countries FROM customers;
